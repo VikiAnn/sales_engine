41e9b9c9d426cfbf787ed4bf1c319aa85495d6fc
Viki Harrod <victoria.harrod@gmail.com>
2014-10-15 22:13:06 -0600

    group_by invoice_id during csv loading

--- lib/invoice_item_parser.rb
+++ lib/invoice_item_parser.rb
@@ -2,7 +2,7 @@ require 'csv'
 require_relative 'invoice_item'
 
 class InvoiceItemParser
-  attr_reader :invoice_items
+  attr_reader :invoice_items, :invoice_items_by_invoice_id
 
   def initialize(repository, filepath)
     @filepath = filepath
@@ -13,10 +13,17 @@ class InvoiceItemParser
     @invoice_items = []
     csv_options = { headers: true,
                     header_converters: :symbol,
-                    converters: :numeric }
+                    converters: :all }
+    @invoice_items_by_invoice_id = {}
 
     CSV.foreach(@filepath, csv_options) do |row|
-      @invoice_items << InvoiceItem.new(repository, row)
+      add_invoice_item(InvoiceItem.new(repository, row))
     end
+  end
+
+  def add_invoice_item(invoice_item)
+    @invoice_items << invoice_item
+    @invoice_items_by_invoice_id[invoice_item.invoice_id] ||= []
+    @invoice_items_by_invoice_id[invoice_item.invoice_id] << invoice_item
   end
 end
